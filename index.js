411c64ae3a029dec9f16085dcc2a81ec1407762d
suyash mandhana <mandhanasuyash@yahoo.in>
2020-03-13 21:13:02 +0530

    committed

--- index.js
+++ index.js
@@ -127,4 +127,6 @@ app.post('/api/message', (req, res) => {
 //   });
 // });
 //
-app.listen(3000, () => console.log('Listening on port 3000'))
+
+var theport = process.env.PORT || 3000;
+app.listen(theport, () => console.log("Listening on port 3000"));
